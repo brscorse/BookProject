e8f42f41b6b6e005a0bdfa8a7c020361ec00b41d
brscorse <brscorse@byu.edu>
2022-02-15 12:28:10 -0700

    Add project files.

---
+++ BookProject/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookProject.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //The number of pages needed
+        public int TotalPages => (int) Math.Ceiling(((double) TotalNumBooks / BooksPerPage));
+
+    }
+}
