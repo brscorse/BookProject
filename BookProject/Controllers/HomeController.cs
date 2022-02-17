c0504db1d6f5a0d61fb732a345f998c5e89b5661
brscorse <brscorse@byu.edu>
2022-02-16 22:36:42 -0700

    Style the Pagination

--- BookProject/Controllers/HomeController.cs
+++ BookProject/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace BookProject.Controllers
         }
         public IActionResult Index(int pageNum = 1)
         {
-            int pageSize = 5;
+            int pageSize = 10;
 
             var x = new BooksViewModel
             {
