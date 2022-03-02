f81e1ece3af772491104829bebfcc5b70f75eb7a
brscorse <brscorse@byu.edu>
2022-03-02 10:48:53 -0700

    Cart Icon

---
+++ BookProject/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using BookProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookProject.Components
+{
+    public class CartSummaryViewComponent : ViewComponent 
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
