8019cd331f7832dfa30751557bf1965e3bbfd9b5
brscorse <brscorse@byu.edu>
2022-03-09 14:05:40 -0700

    Phase 10

--- BookProject/Startup.cs
+++ BookProject/Startup.cs
@@ -45,6 +45,8 @@ namespace BookProject
 
             services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,6 +79,9 @@ namespace BookProject
                 endpoints.MapDefaultControllerRoute();
 
                 endpoints.MapRazorPages();
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
         }
     }
